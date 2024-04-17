98315524c89f99b5139ce14d393502c70ca053b4
Vinicius Gomes <viniciusgarciagomes@hotmail.com>
2024-04-17 16:50:28 -0300

    atualizacao campos
    atualizacao campos

--- FormulárioPDFConverter/Controllers/HomeController.cs
+++ FormulárioPDFConverter/Controllers/HomeController.cs
@@ -100,12 +100,6 @@ namespace FormulárioPDFConverter.Controllers
         {
             try
             {
-                model.InscricaoJuntaComercial = FormatStringField(model.InscricaoJuntaComercial);
-                model.telefoneRepresentante2 = FormatStringField(model.telefoneRepresentante2);
-                model.Complemento = FormatStringField(model.Complemento);
-                model.Bairro = FormatStringField(model.Bairro);
-                model.Site = FormatStringField(model.Site);
-
                 var report = new Rotativa.ActionAsPdf("Ficha", model);
 
                 return report;
@@ -116,14 +110,6 @@ namespace FormulárioPDFConverter.Controllers
                 return Content("Erro ao gerar o arquivo PDF");
             }
         }
-        private string FormatStringField(string fieldValue)
-        {
-            if (string.IsNullOrWhiteSpace(fieldValue))
-            {
-                return fieldValue.PadLeft(10);
-            }
 
-            return fieldValue;
-        }
     }
 }
