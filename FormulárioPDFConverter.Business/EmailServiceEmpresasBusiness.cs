3022417c3feaa84b853a90b04e90879bd73c1e53
Vinicius Gomes <viniciusgarciagomes@hotmail.com>
2024-11-14 15:49:18 -0300

    ajustando text

--- FormulárioPDFConverter.Business/EmailServiceEmpresasBusiness.cs
+++ FormulárioPDFConverter.Business/EmailServiceEmpresasBusiness.cs
@@ -58,7 +58,7 @@ namespace FormulárioPDFConverter.Business
                                         .Where(email => !string.IsNullOrEmpty(email))
                                         .ToList();
 
-                string[] destinatarios = { "vinicius@anfir.org.br", "marcio@anfir.org.br", "christian.hiraya@anfir.org.br", "rodrigo@anfir.org.br" };
+                string[] copiaEmails = { "vinicius@anfir.org.br", "marcio@anfir.org.br", "christian.hiraya@anfir.org.br", "rodrigo@anfir.org.br" };
 
                 string assunto = "Revisão de Documentos - ANFIR";
                 string contexto = MontarMensagem(cnpj, nomeEmpresa, mensagemCorpo, mensagemObservacao, fileName);
